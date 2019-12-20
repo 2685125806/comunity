70ae7657cb0955650e9b82c480802e2e2a642cc0
2685125806 <2685125806@qq.com>
2019-12-20 16:43:28 +0800

    add login from github and change the properties and github app to properties

--- src/main/java/twinkle/CommunityApplication.java
+++ src/main/java/twinkle/CommunityApplication.java
@@ -1,4 +1,4 @@
-package com.twinkle.community.community;
+package twinkle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/twinkle/CommunityApplicationTests.java
+++ src/test/java/twinkle/CommunityApplicationTests.java
@@ -1,4 +1,4 @@
-package com.twinkle.community.community;
+package twinkle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
