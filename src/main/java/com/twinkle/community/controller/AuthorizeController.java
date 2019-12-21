c7374b7778ad309157247dc73288cccae2baf9df
2685125806 <2685125806@qq.com>
2019-12-21 22:52:36 +0800

    add publish.html,add cookie and sesssion check for secound login

--- src/main/java/com/twinkle/community/controller/AuthorizeController.java
+++ src/main/java/com/twinkle/community/controller/AuthorizeController.java
@@ -6,17 +6,19 @@ import com.twinkle.community.mapper.UserMapper;
 import com.twinkle.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.twinkle.community.provider.GitHubProvider;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
 
-@RestController
+@Controller
 public class AuthorizeController {
 
     @Autowired
@@ -50,14 +52,16 @@ public class AuthorizeController {
         GithubUser githubUser = gitHubProvider.getUser(accessToken);
         if (githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insertUser(user);
+            response.addCookie(new Cookie("token",token));
             //登录成功,写cookie和session
-            request.getSession().setAttribute("user",user);
+//            request.getSession().setAttribute("user",user);
             return "redirect:/";
         }else{
             //登录失败重新登录
--- src/main/java/com/twinkle/community/mapper/UserMapper.java
+++ src/main/java/com/twinkle/community/mapper/UserMapper.java
@@ -4,9 +4,15 @@ package com.twinkle.community.mapper;
 import com.twinkle.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 @Mapper
 public interface UserMapper {
 
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insertUser(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
